4abdd284341bde430c54666f32aeb6860f5d183a
Lúcio Nonato <39486464+N0N4T0@users.noreply.github.com>
2021-07-10 10:38:21 -0300

    useEffect

--- 01-fundamentos/myskills/src/pages/Home.js
+++ 01-fundamentos/myskills/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {
   View,
   Text,
@@ -15,15 +15,36 @@ export function Home() {
   const [newSkill, setNewSill] = useState('')
   // estado para armazenar todas as Skills
   const [mySkills, setMySkills] = useState([])
+  // estado para exibir saudação
+  const [greeting, setGreeting] = useState('')
 
   function handleAddSkill(){
     setMySkills(oldState => [...oldState, newSkill])
   }
 
+  useEffect(() => {
+    const currentHour = new Date().getHours()
+
+    if(currentHour < 12){
+      setGreeting('Good Morning!')
+    }
+    else if(currentHour >= 12 && currentHour < 18){
+      setGreeting('Good Afternoon!')
+    }
+    else {
+      setGreeting('Good Night!')
+    }
+
+  }, [])
+
   return(
       <View style={styles.container}>
         <Text style={styles.title}>
           Welcome, Lúcio
+        </Text>
+
+        <Text style={styles.greetings}>
+          {greeting}
         </Text>
 
         <TextInput
@@ -71,5 +92,8 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 30,
     borderRadius: 7
+  },
+  greetings: {
+    color: '#FFF',
   }
 })
