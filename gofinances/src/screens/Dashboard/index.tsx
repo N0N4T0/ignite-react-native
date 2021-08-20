fe6de24c2ce57f2b540c074eb9857cebb2c45e8c
Lúcio Nonato <39486464+N0N4T0@users.noreply.github.com>
2021-08-19 21:29:11 -0300

    utilizando proporções em medidas

--- gofinances/src/screens/Dashboard/index.tsx
+++ gofinances/src/screens/Dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from "react";
-
-import { Container, Title } from './styles';
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
+} from './styles';
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo 
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/39486464?v=4'}}
+                        />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Lúcio</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
