455826408f1e61ecff2b090fe5da2b01cff9cd6d
Lúcio Nonato <39486464+N0N4T0@users.noreply.github.com>
2021-08-19 21:59:29 -0300

    utilizando ícones

--- gofinances/src/screens/Dashboard/index.tsx
+++ gofinances/src/screens/Dashboard/index.tsx
@@ -1,31 +1,34 @@
-import React from "react";
+import React from 'react';
 import {
-    Container,
-    Header,
-    UserWrapper,
-    UserInfo,
-    Photo,
-    User,
-    UserGreeting,
-    UserName
-} from './styles';
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from './styles'
 
 export function Dashboard(){
-    return(
-        <Container>
-            <Header>
-                <UserWrapper>
-                    <UserInfo>
-                        <Photo 
-                            source={{ uri: 'https://avatars.githubusercontent.com/u/39486464?v=4'}}
-                        />
-                        <User>
-                            <UserGreeting>Olá,</UserGreeting>
-                            <UserName>Lúcio</UserName>
-                        </User>
-                    </UserInfo>
-                </UserWrapper>
-            </Header>
-        </Container>
-    )
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/39486464?v=4'}}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Lúcio</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power"/>
+        </UserWrapper>
+      </Header>
+    </Container>
+  )
 }
