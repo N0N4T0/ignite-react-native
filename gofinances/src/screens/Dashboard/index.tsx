8400a488e33f7e47f07f278bac7c2108196393e8
Lúcio Nonato <39486464+N0N4T0@users.noreply.github.com>
2021-08-22 17:53:28 -0300

    criando cartões de transações

--- gofinances/src/screens/Dashboard/index.tsx
+++ gofinances/src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard } from '../../components/TransactionCard'
 
 import {
   Container,
@@ -11,7 +12,9 @@ import {
   UserGreeting,
   UserName, 
   Icon,
-  HighlightCards
+  HighlightCards,
+  Transactions,
+  Title
 } from './styles'
 
 export function Dashboard(){
@@ -53,6 +56,12 @@ export function Dashboard(){
           lastTransaction="01 a 06 de abril"
         />
       </HighlightCards>
+    
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionCard/>
+      </Transactions>
     </Container>
   )
 }
